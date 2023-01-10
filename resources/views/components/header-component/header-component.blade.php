c912a3cacc0ab65db25ccd939f65934416a1b508
nikakh1 <n.khonelidze2@gmail.com>
2023-01-11 02:08:19 +0400

    Add fonts, icons and menu component, added npm

---
+++ resources/views/components/header-component/header-component.blade.php
@@ -0,0 +1,36 @@
+@vite(['resources/views/components/header-component/header-component.scss'])
+
+<div class="nav">
+    @php
+        $menu_items = ['ტაძრის შესახებ', 'გალერეა', 'კონტაქტი', 'შემოწირე'];
+        $nav_logo_title = 'ზუგდიდის ივერიის ყოვლაწმინდა ღვთისმშობლის სახელობის საკათედრო ტაძარი';
+    @endphp
+
+    @for ($i = 0; $i < count($menu_items); $i++)
+        @if($i === count($menu_items) - 1)
+            <div class="nav__item nav__item-highlighted">
+                {{ $menu_items[$i] }}
+            </div>
+        @else
+            <div class="nav__item">
+                {{ $menu_items[$i] }}
+            </div>
+        @endif
+    @endfor
+
+    <div class="nav__logo">
+        <div class="nav__logo__title">
+            {{ $nav_logo_title }}
+        </div>
+        <div class="nav__logo__icon">
+            <img src="{{URL::asset('/assets/icons/nav_icon.png')}}" alt="Altar logo">
+        </div>
+    </div>
+
+    <div class="nav__language">
+        <select class="nav__language__select" name="" id="">
+            <option value="eng">ENG</option>
+            <option value="geo">ქარ</option>
+        </select>
+    </div>
+</div>
