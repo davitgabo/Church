50950605011889bcc6d4a65f35a361cc9d044087
davit <datigabashvili@gmail.com>
2023-01-20 15:37:09 +0400

    modify about page to dynamic text

--- resources/views/components/about-info-component/about-info-component.blade.php
+++ resources/views/components/about-info-component/about-info-component.blade.php
@@ -8,10 +8,10 @@
 
                 <div class="about-info__text-section__text">
                     <div class="about-info__text-section__text-one">
-                        რომელიც განკუთვნილია დაფიქრდით პროექტის მიმდინარეობაზე და გააცანით იგი ფართო საზოგადოებას. სამეგრელოში პირველად შენდება ტაძარი, რომელიც ერთ-ერთი მათგანია ეს იქნება ყველაზე მასშტაბური პროექტი საქართველოში
+                        {{$contents['about text'][0]['text']}}
                     </div>
                     <div class="about-info__text-section__text-two">
-                        რომელიც განკუთვნილია დაფიქრდით პროექტის მიმდინარეობაზე და გააცანით იგი ფართო საზოგადოებას. სამეგრელოში პირველად შენდება ტაძარი, რომელიც ერთ-ერთი მათგანია ეს იქნება ყველაზე მასშტაბური პროექტი საქართველოში
+                        {{$contents['about text'][1]['text']}}
                     </div>
                 </div>
             </div>
@@ -19,10 +19,10 @@
         <div class="col-xl-6">
             <div class="about-info__second-section">
                 <div class="about-info__second-section__title">
-                    ტაძრის მშენებლობა
+                    {{$contents['picture title'][0]['text']}}
                 </div>
                 <div class="about-info__second-section__title-two">
-                    - სულიერი გადარჩენის საწინდარია
+                    {{$contents['picture title'][1]['text']}}
                 </div>
                 <div class="about-info__second-section__img" style="background-image: url('{{ URL::asset('/assets/icons/dl.beatsnoop 2.png')}}')"></div>
             </div>
