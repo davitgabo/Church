b6026e967dbb287df802cb50eccdef4b9da40e18
nikakh1 <n.khonelidze2@gmail.com>
2023-01-20 21:00:38 +0400

    add form to contact page

--- resources/views/components/contact-component/contact-component.blade.php
+++ resources/views/components/contact-component/contact-component.blade.php
@@ -1,3 +1,10 @@
 <div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+    <form action="">
+        <input style="border: 1px solid black" type="text" name="name">
+        <input style="border: 1px solid black" type="text" name="surname">
+        <input style="border: 1px solid black" type="text" name="email">
+        <input style="border: 1px solid black" type="text" name="subject">
+        <textarea style="border: 1px solid black" name="text" id="" cols="30" rows="10"></textarea>
+        <button type="submit" class="btn btn-primary">submit</button>
+    </form>
 </div>
