8a3210eafa007e4f20faaf650dd5ac81ecff8a4f
davit <datigabashvili@gmail.com>
2023-02-02 14:12:39 +0400

    clean up code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -9,6 +10,12 @@ use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
+    /**
+     * log in function
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function login(Request $request)
     {
         $credentials = $request->only('name', 'password');
@@ -20,6 +27,11 @@ class AuthController extends Controller
         return redirect()->back();
     }
 
+    /**
+     * log out function
+     *
+     * @return RedirectResponse
+     */
     public function logout()
     {
         Auth::logout();
@@ -27,22 +39,29 @@ class AuthController extends Controller
         return to_route('login');
     }
 
+    /**
+     * change admin password
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function changePassword(Request $request)
     {
         $request->validate([
             'current_password' => 'required',
-            'password' => 'required|confirmed',
+            'password' => 'required|confirmed|min:6',
         ]);
 
-        if (Hash::check($request->input('current_password'), Auth::user()->password)) {
-            $user = Auth::user();
-            $user->password = Hash::make($request->input('password'));
-            $user->save();
+        $user = Auth::user();
 
-            return redirect()->back()->with("success","Password changed successfully !");
-        } else {
+        if (!Hash::check($request->input('current_password'), $user->password)) {
             return redirect()->back()->with("error","Current password does not match !");
         }
+
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
+
+        return redirect()->back()->with("success","Password changed successfully !");
     }
 
 }
--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -3,29 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Content;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 
 class ContentController extends Controller
 {
+    /**
+     * edit record in content table
+     *
+     * @param Request $request
+     * @param $id
+     * @return RedirectResponse
+     */
     public function edit(Request $request, $id)
     {
-        $inputs = $request->validate(['text_en'=>'required|string|max:1800',
-                                      'text_ge'=>'required|string|max:1800' ]);
+        $request->validate([
+            'text_en' => 'required|string|max:1800',
+            'text_ge' => 'required|string|max:1800'
+        ]);
+
         $content = Content::find($id);
 
-        if($content){
-            $content->text = strip_tags($inputs['text_en']);
-            $content->text_ge = strip_tags($inputs['text_ge']);
-            $content->save();
+        if (!$content) {
+            return redirect()->route('dashboard')->with('error', 'Content not found');
         }
-        return to_route('dashboard');
+
+        $content->text = htmlentities($request->input('text_en'));
+        $content->text_ge = htmlentities($request->input('text_ge'));
+        $content->save();
+
+        return redirect()->back()->with('success', 'Content updated successfully');
     }
 
+    /**
+     * delete slider
+     *
+     * @param $id
+     * @return RedirectResponse
+     */
     public function delete($id)
     {
         $slider = Content::find($id);
+
         if (!$slider) {
             Log::error("Content with ID $id not found");
             return redirect()->back();
@@ -50,50 +71,75 @@ class ContentController extends Controller
         return redirect()->back();
     }
 
+    /**
+     * store new slider
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
     public function store(Request $request)
     {
-        $inputs = $request->validate([
-            'text_ge'=>'required|string|max:1800',
-            'text_en'=>'required|string|max:1800',
-            'image'=>'required|image'
+        $validatedData = $request->validate([
+            'text_ge' => 'required|string|max:1800',
+            'text_en' => 'required|string|max:1800',
+            'image' => 'required|image'
         ]);
+
         $slider = new Content();
-        if ($slider->isStorable()){
-            $image = $request->file('image');
-            $slider->text = strip_tags($inputs['text_en']);
-            $slider->text_ge = strip_tags($inputs['text_ge']);
-            $slider->section = 'slider';
-            $slider->page = 'home';
-            $slider->description = 'სლაიდერის ტექსტი';
-            $slider->uri = time().$image->getClientOriginalName();
-            if ( $slider->save() ){
-                $image->move(public_path().'/assets/images',$slider->uri);
-            }
+        if (!$slider->isStorable()) {
+            return redirect()->back();
         }
+
+        $image = $request->file('image');
+        $slider->text = strip_tags($validatedData['text_en']);
+        $slider->text_ge = strip_tags($validatedData['text_ge']);
+        $slider->section = 'slider';
+        $slider->page = 'home';
+        $slider->description = 'სლაიდერის ტექსტი';
+        $slider->uri = time().$image->getClientOriginalName();
+
+        if ($slider->save()) {
+            $image->move(public_path().'/assets/images', $slider->uri);
+        }
+
         return redirect()->back();
     }
 
+    /**
+     * change slider and about images
+     *
+     * @param Request $request
+     * @param $id
+     * @return RedirectResponse
+     */
     public function changeImage(Request $request, $id)
     {
-        $request->validate(['image'=>'required|image']);
+        $request->validate(['image' => 'required|image']);
         $content = Content::find($id);
 
-        if ($content->hasPicture($content->section)){
-            $oldImage = $content->uri;
-            $image = $request->file('image');
-            $content->uri = time().$image->getClientOriginalName();
-            try {
-                if ($content->save()){
-                    $image->move(public_path().'/assets/images', $content->uri);
-                    $file_path = public_path("assets/images/$oldImage");
-                    if (File::exists($file_path)) {
-                        File::delete($file_path);
-                    }
-                }
-            }   catch (\Exception $e) {
-                    Log::error("Error deleting content with ID $id: " . $e->getMessage());
-                }
+        if (!$content) {
+            return redirect()->back()->withErrors(['Content not found']);
         }
+
+        $image = $request->file('image');
+        $imageName = time() . $image->getClientOriginalName();
+        $imagePath = public_path("assets/images/$imageName");
+
+        try {
+            if ($content->update([
+                'uri' => $imageName,
+            ])) {
+                $image->move(public_path().'/assets/images', $imageName);
+
+                if ($content->uri && file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+            }
+        } catch (\Exception $e) {
+            Log::error("Error updating content with ID $id: " . $e->getMessage());
+            return redirect()->back()->withErrors(['Error updating content']);
+        }
+
         return redirect()->back();
     }
 }
