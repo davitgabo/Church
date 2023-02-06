f1473f65f0753ddf3a53049a7e96778e3a6286d2
davit <datigabashvili@gmail.com>
2023-02-06 11:41:48 +0400

    changes to imagecontroller

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,8 +12,8 @@ class ImageController extends Controller
     public function store(Request $request)
     {
         $inputs = $request->validate([
-            'desc_ge'=>'required|string|max:1800',
-            'desc_en'=>'required|string|max:1800',
+            'desc_ge'=>'nullable|string|max:1800',
+            'desc_en'=>'nullable|string|max:1800',
             'image'=>'required|image'
         ]);
 
@@ -51,16 +51,16 @@ class ImageController extends Controller
 
     public function change(Request $request, $id)
     {
-        $inputs = $request->validate(['desc_en'=>'required|string|max:1800',
-                                      'desc_ge'=>'required|string|max:1800',
+        $inputs = $request->validate(['desc_en'=>'nullable|string|max:1800',
+                                      'desc_ge'=>'nullable|string|max:1800',
                                       'image'=>'image']);
         $image = Image::find($id);
         if (!$image) {
             Log::error("Content with ID $id not found");
             return redirect()->back();
         }
-        $image->desc_en = strip_tags($inputs['text_en']);
-        $image->desc_ge = strip_tags($inputs['text_ge']);
+        $image->desc_en = strip_tags($inputs['desc_en']);
+        $image->desc_ge = strip_tags($inputs['desc_ge']);
 
         if( $request->hasFile('image') ){
             $picture = $request->file('image');
@@ -68,6 +68,6 @@ class ImageController extends Controller
             $picture->move(public_path().'/assets/images',$image->name);
         }
         $image->save();
-        return to_route('dashboard');
+        return redirect()->back();
     }
 }
