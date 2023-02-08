9503f9bbba394f3a48c1ddad7b088eb2b319d154
davit <datigabashvili@gmail.com>
2023-02-08 11:59:26 +0400

    commit to pull

--- resources/views/components/admin-donations/admin-donations.blade.php
+++ resources/views/components/admin-donations/admin-donations.blade.php
@@ -1,3 +1,19 @@
 <div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+    <div>
+        <h1> დაუდასტურებელი</h1>
+        @foreach($donations as $donation)
+            @if($donation->status == 'pending')
+                <div>
+                    გადმომრიცხველი: {{$donation->first_name}} {{$donation->last_name}}
+                    თანხა: {{$donation->amount}}
+                    ერთჯერადი კოდი: {{$donation->payment_title}}
+                </div>
+                <form action="/donations/change" method="post">
+                    @csrf
+                    @method('PUT')
+                    <input type="submit" value="">
+                </form>
+            @endif
+        @endforeach
+    </div>
 </div>
