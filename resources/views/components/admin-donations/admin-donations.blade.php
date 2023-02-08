12088c285d408a8bf3c95688a4d80e5a64bbf620
davit <datigabashvili@gmail.com>
2023-02-08 13:58:15 +0400

    add routes and donation divs in blade

--- resources/views/components/admin-donations/admin-donations.blade.php
+++ resources/views/components/admin-donations/admin-donations.blade.php
@@ -1,17 +1,61 @@
 <div>
     <div>
-        <h1> დაუდასტურებელი</h1>
+        <h3> დაუდასტურებელი</h3>
         @foreach($donations as $donation)
             @if($donation->status == 'pending')
                 <div>
                     გადმომრიცხველი: {{$donation->first_name}} {{$donation->last_name}}
                     თანხა: {{$donation->amount}}
                     ერთჯერადი კოდი: {{$donation->payment_title}}
+                    კომენტარი: {{$donation->comment}}
                 </div>
-                <form action="/donations/change" method="post">
+                <form action="/donations/status/{{$donation->id}}" method="post">
                     @csrf
                     @method('PUT')
-                    <input type="submit" value="">
+                    <button type="submit" value="1" name="approved"> approve </button>
+                    <button type="submit" value="0" name="approved"> reject </button>
+                </form>
+            @endif
+        @endforeach
+    </div>
+    <div>
+        <h3> დადასტურებული</h3>
+        @foreach($donations as $donation)
+            @if($donation->status == 'approved')
+                <div>
+                    გადმომრიცხველი: {{$donation->first_name}} {{$donation->last_name}}
+                    თანხა: {{$donation->amount}}
+                    ერთჯერადი კოდი: {{$donation->payment_title}}
+                    კომენტარი: {{$donation->comment}}
+                </div>
+                <form action="/donations/status/{{$donation->id}}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <button type="submit" value="0" name="approved"> reject </button>
+                </form>
+            @endif
+        @endforeach
+    </div>
+
+    <div>
+        <h3> უარყოფილი</h3>
+        @foreach($donations as $donation)
+            @if($donation->status == 'rejected')
+                <div>
+                    გადმომრიცხველი: {{$donation->first_name}} {{$donation->last_name}}
+                    თანხა: {{$donation->amount}}
+                    ერთჯერადი კოდი: {{$donation->payment_title}}
+                    კომენტარი: {{$donation->comment}}
+                </div>
+                <form action="/donations/status/{{$donation->id}}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <button type="submit" value="1" name="approved"> approve </button>
+                </form>
+                <form action="/donations/delete/{{$donation->id}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"> DELETE </button>
                 </form>
             @endif
         @endforeach
